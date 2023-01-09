44f0fb420ed6a5e2483d812158a39449654b4a5a
KONDABOINARAVI <ravirampaul1994@gmail.com>
2023-01-09 17:02:29 +0530

    my first commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2 class=""Underline>"Underline"</h2>
+</template>
+
+<script> 
+import CreatePost from './components/CreatePost.vue'
+
+
+export default {
+  name: 'App',
+  components: { 
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.underline{
+  text-decoration: underline;
+}
+</style>
